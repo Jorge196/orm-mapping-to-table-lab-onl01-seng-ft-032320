c4d3fe064f7f3ef9d4389d6d66535a4012be9cfe
Jorge Ramirez <joramirez96@gmail.com>
2020-04-23 22:17:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,5 +9,14 @@ attr_reader :id
     @id = id
   end
 
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+    SQL
+      DB[:conn].execute(sql)
+  end 
 end
